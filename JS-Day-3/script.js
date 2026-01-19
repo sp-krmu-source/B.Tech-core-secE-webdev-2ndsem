ab9c96b25074885372ae2e972aff90ca405fd546
S.P Acharya <spacharya@SPs-MacBook-Air.local>
2026-01-19 13:32:42 +0530

    session 3 js

---
+++ JS-Day-3/script.js
@@ -0,0 +1,21 @@
+//  let fruits = ["apple", "banana"];
+//  let newLength = fruits.push("cherry")
+//  console.log(newLength);
+//  console.log(fruits)
+
+
+//  let fruits = ["apple", "banana", "cherry"];
+//  let removed = fruits.pop();
+//  console.log(removed);
+//  console.log(fruits)
+
+
+// Q1. 
+let arr = [1,2,3,4,5]
+let reverse=[]
+
+while(arr.length > 0){
+    reverse.push(arr.pop())
+} 
+
+console.log(reverse);
